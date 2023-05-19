3a4307cf3fbc12340d97235322bb271e5da77cff
John Jackson <jdjacks@post.harvard.edu>
2023-05-19 10:29:45 -0700

    cleanup prototype code

--- src/napari_cool_tools_segmentation/_segmentation.py
+++ src/napari_cool_tools_segmentation/_segmentation.py
@@ -40,37 +40,6 @@ def segmentation_inference(img:Image, state_dict_path=Path("D:\JJ\Development\Ch
     gen.load_state_dict(state_dict)
     gen.eval()
 
-    '''
-    pt_data = pt_data.unsqueeze(0).repeat(3,1,1)
-
-    output = gen(pt_data)
-
-    retina = output[0] == 1
-    choroid = output[1] == 1
-    sclera = output[2] == 1
-
-    labels = torch.empty_like(output[0])
-    labels[retina] = 1
-    labels[choroid] = 2
-    labels[sclera] = 3
-    labels = labels.to(torch.uint8)
-    labels = labels.detach().cpu().numpy()
-
-    output = output.detach().cpu().numpy()
-
-    name = f"{img.name}_Seg"
-    add_kwargs = {"name":f"{name}"}
-
-    if label_flag:
-        layer_type = 'labels'
-        layer = Layer.create(labels,add_kwargs,layer_type)
-    else:
-        layer_type = 'image'
-        layer = Layer.create(output,add_kwargs,layer_type)
-
-    return layer
-    '''
-
     try:
         assert data.ndim == 2 or data.ndim == 3, "Only works for data of 2 or 3 dimensions"
     except AssertionError as e:
